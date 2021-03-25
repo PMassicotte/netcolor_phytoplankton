03dbd91afea0a90d62a26259e2015659f56b7b4f
Philippe Massicotte <pmassicotte@hotmail.com>
2021-03-25 07:48:36 -0400

    Add plot for aphy_specific.

--- R/07_explore_absorption.R
+++ R/07_explore_absorption.R
@@ -8,16 +8,8 @@ rm(list = ls())
 
 source("R/zzz.R")
 
-absorption <- fread(here("data/clean/absorption.csv")) %>%
+absorption <- fread(here("data/clean/merged_dataset.csv")) %>%
   as_tibble()
-
-bioregions <- read_csv(here("data/clean/bioregions.csv"))
-
-absorption %>%
-  distinct(sample_id)
-
-absorption <- absorption %>%
-  inner_join(bioregions, by = "sample_id")
 
 # Plot aphy ---------------------------------------------------------------
 
@@ -39,6 +31,31 @@ p <- absorption %>%
 
 ggsave(
   here("graphs/07_aphy_spectral_profiles_per_bioregion.pdf"),
+  device = cairo_pdf,
+  width = 8,
+  height = 5
+)
+
+# Plot aphy specific ------------------------------------------------------
+
+p <- absorption %>%
+  ggplot(aes(x = wavelength, y = aphy_specific, group = sample_id, color = bioregion_name)) +
+  geom_line(size = 0.1) +
+  scale_color_manual(
+    breaks = area_breaks,
+    values = area_colors
+  ) +
+  labs(
+    x = "Wavelength (nm)",
+    y = quote(a[phy]^"*" ~ (m^{-1}))
+  ) +
+  facet_wrap(~bioregion_name, scales = "free_y") +
+  theme(
+    legend.position = "none"
+  )
+
+ggsave(
+  here("graphs/07_aphy_specific_spectral_profiles_per_bioregion.pdf"),
   device = cairo_pdf,
   width = 8,
   height = 5
@@ -93,3 +110,6 @@ ggsave(
   width = 8,
   height = 5
 )
+
+
+absorption
