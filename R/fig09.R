195c43b45b3c7166e8eff4bcb89bfbeb934208aa
Philippe Massicotte <pmassicotte@hotmail.com>
2021-10-28 08:31:48 -0400

    Fig. 9 and appendix 4 are merged together.

--- R/fig09.R
+++ R/fig09.R
@@ -14,8 +14,15 @@ rm(list = ls())
 
 aphy <- read_csv(here("data", "clean", "merged_dataset.csv")) %>%
   filter(wavelength %in% c(443, 675)) %>%
-  select(sample_id, bioregion_name, wavelength, season, aphy)
-
+  select(
+    sample_id,
+    bioregion_name,
+    wavelength,
+    season,
+    aphy,
+    aphy_specific,
+    hplcchla
+  )
 
 paaw <- read_csv(here("data", "clean", "apparent_visible_wavelength.csv")) %>%
   select(sample_id, bioregion_name, avw_aphy)
@@ -24,12 +31,124 @@ df <- inner_join(aphy, paaw, by = c("sample_id", "bioregion_name"))
 
 df
 
+# chla vs paaw ------------------------------------------------------------
+
+p1 <- df %>%
+  filter(wavelength == 443) %>%
+  ggplot(aes(x = avw_aphy, y = hplcchla)) +
+  geom_point(
+    aes(fill = season),
+    color = "transparent",
+    size = 1.5,
+    stroke = 0,
+    pch = 21,
+    alpha = 0.3
+  ) +
+  scale_fill_manual(
+    breaks = c("Winter", "Spring", "Summer", "Autumn"),
+    values = c("#014f86", "#40916c", "#ffcb69", "#e76f51"),
+    guide = guide_legend(
+      override.aes = list(size = 2, alpha = 1),
+      label.theme = element_text(size = 8, family = "Montserrat")
+    )
+  ) +
+  scale_x_continuous(breaks = scales::breaks_pretty()) +
+  scale_y_log10() +
+  annotation_logticks(sides = "l", size = 0.1) +
+  geom_smooth(
+    color = "#3c3c3c",
+    size = 0.5,
+    alpha = 0.25,
+    method = "lm"
+  ) +
+  labs(
+    x = NULL,
+    y = quote(Chlorophyll-italic(a)~(mg~m^{-3}))
+  ) +
+  ggpmisc::stat_poly_eq(
+    aes(label = ..eq.label..),
+    parse = TRUE,
+    coef.digits = 4,
+    f.digits = 5,
+    p.digits = 10,
+    label.x.npc = 0.05,
+    family = "Montserrat",
+    size = 3
+  ) +
+  ggpmisc::stat_poly_eq(
+    label.x.npc = 0.05,
+    label.y.npc = 0.88,
+    aes(label = ..rr.label..),
+    size = 3,
+    family = "Montserrat"
+  ) +
+  theme(
+    legend.title = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.98, 0.02),
+    legend.key.size = unit(0.4, "cm"),
+    legend.background = element_blank()
+  )
+
+# aphy* vs paaw -----------------------------------------------------------
+
+p2 <- df %>%
+  filter(wavelength == 443) %>%
+  ggplot(aes(x = avw_aphy, y = aphy_specific)) +
+  geom_point(
+    aes(fill = season),
+    color = "transparent",
+    size = 1.5,
+    stroke = 0,
+    pch = 21,
+    alpha = 0.3
+  ) +
+  scale_fill_manual(
+    breaks = c("Winter", "Spring", "Summer", "Autumn"),
+    values = c("#014f86", "#40916c", "#ffcb69", "#e76f51")
+  ) +
+  scale_x_continuous(breaks = scales::breaks_pretty()) +
+  scale_y_log10() +
+  annotation_logticks(sides = "l", size = 0.1) +
+  geom_smooth(
+    color = "#3c3c3c",
+    size = 0.5,
+    alpha = 0.25,
+    method = "lm"
+  ) +
+  labs(
+    x = NULL,
+    y = quote(a[phi]^'*'~(443)~(m^2~mg^{-1}))
+  ) +
+  ggpmisc::stat_poly_eq(
+    aes(label = ..eq.label..),
+    parse = TRUE,
+    coef.digits = 4,
+    f.digits = 5,
+    p.digits = 10,
+    label.x.npc = 1,
+    label.y.npc = 0.88,
+    family = "Montserrat",
+    size = 3
+  ) +
+  ggpmisc::stat_poly_eq(
+    label.x.npc = 1,
+    label.y.npc = 0.8,
+    aes(label = ..rr.label..),
+    size = 3,
+    family = "Montserrat"
+  ) +
+  theme(
+    legend.position = "none"
+  )
+
 # Relation between aphy ratio and PAAW ------------------------------------
 
 # It was shown that the aphy* 443/675 could be related to both as a function of
 # cell size and irradiance (Fujiki2002).
 
 df_viz <- df %>%
+  select(-aphy_specific, -hplcchla) %>%
   pivot_wider(
     names_from = wavelength,
     values_from = aphy,
@@ -41,7 +160,7 @@ df_viz
 
 formula <- y ~ x + I(x^2)
 
-p <- df_viz %>%
+p3 <- df_viz %>%
   ggplot(aes(x = avw_aphy, y = aphy_ratio)) +
   geom_point(
     aes(fill = season),
@@ -57,6 +176,7 @@ p <- df_viz %>%
     size = 0.5,
     alpha = 0.25
   ) +
+  scale_x_continuous(breaks = scales::breaks_pretty()) +
   ggpmisc::stat_poly_eq(
     formula = formula,
     aes(label = ..eq.label..),
@@ -65,12 +185,13 @@ p <- df_viz %>%
     f.digits = 5,
     p.digits = 10,
     label.x.npc = 1,
+    label.y.npc = 0.88,
     family = "Montserrat",
     size = 3
   ) +
   ggpmisc::stat_poly_eq(
     formula = formula,
-    label.y.npc = 0.88,
+    label.y.npc = 0.8,
     label.x.npc = 1,
     aes(label = ..rr.label..),
     size = 3,
@@ -78,30 +199,15 @@ p <- df_viz %>%
   ) +
   scale_fill_manual(
     breaks = c("Winter", "Spring", "Summer", "Autumn"),
-    values = c("#014f86", "#40916c", "#ffcb69", "#e76f51"),
-    guide = guide_legend(
-      override.aes = list(size = 2, alpha = 1),
-      label.theme = element_text(size = 8, family = "Montserrat")
-    )
+    values = c("#014f86", "#40916c", "#ffcb69", "#e76f51")
   ) +
   labs(
     x = "Phytoplankton Apparent Absorption Wavelength (PAAW, nm)",
     y = quote(a[phi] ~ (443) / a[phi] ~ (675))
   ) +
-  # facet_wrap(~bioregion_name, ncol = 1) +
   theme(
-    legend.title = element_blank(),
-    legend.justification = c(0, 0),
-    legend.position = c(0.02, 0.02),
-    legend.key.size = unit(0.4, "cm")
+    legend.position = "none"
   )
-
-ggsave(
-  here("graphs", "fig09.pdf"),
-  device = cairo_pdf,
-  width = 5,
-  height = 4
-)
 
 ## Model to predict aphy* ratio from PAAW ----
 
@@ -129,3 +235,18 @@ df_viz %>%
   arrange(mean_aphy_ratio)
 
 range(df_viz$aphy_ratio)
+
+# Save plots --------------------------------------------------------------
+
+p <- p1 / p2 / p3 +
+  plot_annotation(tag_levels = "A") &
+  theme(
+    plot.tag = element_text(face = "bold")
+  )
+
+ggsave(
+  here("graphs", "fig09.pdf"),
+  device = cairo_pdf,
+  width = 5,
+  height = 8
+)
