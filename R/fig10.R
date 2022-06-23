4c6effe46fd2baf6f0487328ee6d5735966768bf
Philippe Massicotte <pmassicotte@hotmail.com>
2022-06-23 07:24:06 -0400

    Decadal visualization iof chla.

--- R/fig10.R
+++ R/fig10.R
@@ -19,7 +19,8 @@ df <- open_dataset(here("data", "clean", "merged_dataset")) |>
     date,
     aphy,
     anap,
-    snap
+    snap,
+    hplcchla
   ) |>
   collect()
 
@@ -36,7 +37,7 @@ df <- df |>
 df_viz <- df |>
   mutate(year = lubridate::year(date)) |>
   group_by(bioregion_name, year, season) |>
-  summarise(across(c(aphy, anap, snap), mean), n = n()) |>
+  summarise(across(c(aphy, anap, snap, hplcchla), mean), n = n()) |>
   ungroup() |>
   filter(n >= 10)
 
@@ -48,7 +49,9 @@ df_viz <- df_viz |>
 df_viz <- df_viz |>
   filter(season %in% c("Autumn", "Spring"))
 
-make_plot <- function(df, var, ylab) {
+make_plot <- function(df, var, ylab, free_y = FALSE) {
+  scale_y <- ifelse(free_y, "free_y", "fixed")
+
   df %>%
     ggplot(aes(x = year, y = {{ var }})) +
     geom_point(aes(color = season, pch = bioregion_name)) +
@@ -96,7 +99,7 @@ make_plot <- function(df, var, ylab) {
       x = NULL,
       y = parse(text = ylab)
     ) +
-    facet_grid(season ~ str_wrap_factor(bioregion_name, 20), scales = "fixed") +
+    facet_grid(season ~ str_wrap_factor(bioregion_name, 20), scales = scale_y) +
     theme(
       legend.position = "none",
       panel.spacing = unit(1, "lines", data = NULL),
@@ -108,6 +111,7 @@ p1 <- make_plot(df_viz, aphy, "a[phi]~(443)~(m^{-1})")
 p2 <- make_plot(df_viz, anap, "a[NAP]~(443)~(m^{-1})")
 p3 <- make_plot(df_viz, snap, "s[NAP]~(443)~(nm^{-1})")
 p4 <- make_plot(df_viz, anap / aphy, "a[NAP]~(443) / a[phi]~(443)")
+p5 <- make_plot(df_viz, hplcchla, "'[Chl-a]'~(mg~m^{-3})", free_y = TRUE)
 
 ggsave(
   here("graphs/fig10a.pdf"),
@@ -116,6 +120,7 @@ ggsave(
   width = 7,
   height = 4.7
 )
+
 ggsave(
   here("graphs/fig10b.pdf"),
   p2,
@@ -123,6 +128,7 @@ ggsave(
   width = 7,
   height = 4.7
 )
+
 ggsave(
   here("graphs/fig10c.pdf"),
   p3,
@@ -130,9 +136,18 @@ ggsave(
   width = 7,
   height = 4.7
 )
+
 ggsave(
   here("graphs/fig10d.pdf"),
   p4,
+  device = cairo_pdf,
+  width = 7,
+  height = 4.7
+)
+
+ggsave(
+  here("graphs/fig10e.pdf"),
+  p5,
   device = cairo_pdf,
   width = 7,
   height = 4.7
